6a67f32a8b7887648df5a6db5015b7f4c5c9e606
Daniel Commins <dencee@users.noreply.github.com>
2019-06-25 14:31:57 -0700

    Zayden's section 2

---
+++ src/section2_zayden/PentagonCrazy.java
@@ -0,0 +1,50 @@
+package section2;
+
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
+/* Teacher’s note: before beginning, draw a pentagon and have students work out the angle that the robot will have to turn (360/5) */
+
+public class PentagonCrazy {
+
+	private void makePrettyThings() {
+		// 1. Create a new Robot
+Robot op= new Robot();
+		// 3. Put the robot's pen down
+op.penDown();
+op.setSpeed(99999);
+op.miniaturize();
+// 8. Make the robot go at maximum speed (100)
+
+		// 9. Set the pen to a color that you like for the shape
+
+		// 4. Make a variable for the number of sides you want (can’t test this one)
+int num=5;
+		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
+int ang=72;
+		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you should see a pentagon
+for (int i = 0; i < 200; i++) {
+	
+
+			// 2. Move the robot 200 pixels
+	op.setRandomPenColor();
+	op.move(i);
+			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
+	
+			// 6. Turn the robot the amount in your angle variable
+	op.turn(ang);
+			// 11. Turn the robot one more degree
+op.turn(1);
+}
+	}
+
+	// Variations:
+	// *12. make the pattern really huge
+	// *13. randomize the color of the pattern 
+	// *14. experiment with different shapes
+
+	public static void main(String[] args) {
+		new PentagonCrazy().makePrettyThings();
+	}
+}
---
+++ src/section2_zayden/Zaydens_creation.java
@@ -0,0 +1,29 @@
+package section2;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Zaydens_creation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Robot a=new Robot();
+	
+Robot b=new Robot();
+		Robot c=new Robot();
+		Robot d=new Robot();
+		a.setSpeed(100);
+		b.setSpeed(100);
+		c.setSpeed(100);
+		d.setSpeed(100);
+		b.turn(90);
+	c.turn(180);
+	d.turn(270);
+	a.move(100);
+	b.move(100);
+	c.move(100);
+	d.move(100);
+	
+	
+	}
+
+}
