86fc9a49c68d931f485a3c5bb95b95feb536fc2e
Daniel Commins <dencee@users.noreply.github.com>
2019-10-16 18:33:09 -0700

    jack section3

---
+++ src/section3_Jack/HelloWorld.java
@@ -0,0 +1,24 @@
+
+
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld 
+{public static void main(String[] args) {
+System.out.println("abcdefg");
+JOptionPane.showMessageDialog(null,"sharks are delilhdcntfhh");
+String str="sharks are delocius ilike";
+System.out.println(str);
+String str2=JOptionPane.showInputDialog("penguin");
+System.out.println(str2);
+
+
+
+
+
+
+
+}
+
+}
